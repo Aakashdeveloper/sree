b52b6b0491d52c90d5cdae42b245570d3efddf21
AAKASH HANDA <AAKASH HANDA>
2017-04-27 09:14:55 +0530

    sdfsdf

---
+++ first.js
@@ -0,0 +1,78 @@
+string 
+number
+boolen
+null
+undfiend 
+
+//Es5
+var a = "sree";
+var a = 10;
+var a = true;
+
+//Es6
+const b = 10;
+let c =10;
+
+var arr = [];
+var arr = [1,2,3,1,4];
+var arbbr = ['a','c','c','d'];
+var jarr= [1,1,1,'m','f',1465,true,'jhzsh'];
+var nest= [1,2,[3,4,[5,6],9],6,7];
+
+
+var g = a.split(" ")
+undefined
+g
+(4) ["hi", "i", "am", "here"]
+g[0]
+"hi"
+
+
+
+var h = "https://global.gotomeeting.com/join/signin/kjxcvj/412623909"
+undefined
+h.split("/")[r.length-1]
+"kjxcvj"
+var r = h.split('/')
+undefined
+h.split("/")[r.length-1]
+"412623909"
+
+10+10
+20
+"sree" + " nodejs"
+"sree nodejs"
+10+10+"10"
+"2010"
+10+"10"+10
+"101010"
+"10"+10+10
+"101010"
+"10"+(10+10)
+"1020"
+
+
+function add(a,b){
+	return a+b;
+} 
+
+add(1,2)
+
+
+function isEvan(a){
+	if(a%2==0){
+		console.log("number is even");
+	}else{
+		console.log("number is odd");
+	}
+}
+
+
+
+
+
+
+
+
+
+
