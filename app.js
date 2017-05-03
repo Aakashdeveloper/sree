a85ad4a1a0c1006df043b071895490102a007d06
AAKASH HANDA <AAKASH HANDA>
2017-05-03 06:01:45 +0530

    a

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static(__dirname+'/public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req,res){
-	res.send("hello i am here")
+	//res.send("hello i am here");
+	res.render('index', {title:"hello i am new",list:["home","about","contact"]})
 });
 
 app.get('/books',function(req,res){
-	res.send("hello books")
+	res.send("hello books");
 })
 
 
