58bae93eb581412d53e5328847f67a066f081224
AAKASH HANDA <AAKASH HANDA>
2017-05-04 05:58:13 +0530

    a

--- app.js
+++ app.js
@@ -3,24 +3,66 @@ var express = require('express');
 var app = express();
 
 var port = 5000;
+var bookRouter = express.Router();
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use('/Books', bookRouter);
 
+bookRouter.route('/')
+    .get(function(res, res){
+      res.render('bookListView', {
+    title: "NodeJs",
+    nav: [{
+      Links: '/Books',
+      Text: 'Boook'
+    }, {
+      Links: '/Authors',
+      Text: 'Authors'
+    }]
+   });
+  });
 
-app.get('/', function(req,res){
-	//res.send("hello i am here");
-	res.render('index', {title:"hello i am new",list:["home","about","contact"]})
+bookRouter.route('/single')
+    .get(function(req,res){
+      res.send("i am single book")
+    });
+
+app.get('/Authors', function(req, res) {
+  //res.send("hello i am here");
+  res.render('author', {
+    title: "NodeJs",
+    nav: [{
+      Links: '/Books',
+      Text: 'Boook'
+    }, {
+      Links: '/Authors',
+      Text: 'Authors'
+    }]
+  });
+});
+app.get('/', function(req, res) {
+  //res.send("hello i am here");
+  res.render('index', {
+    title: "NodeJs",
+    nav: [{
+      Links: '/Books',
+      Text: 'Boook'
+    }, {
+      Links: '/Authors',
+      Text: 'Authors'
+    }]
+  });
 });
 
-app.get('/books',function(req,res){
-	res.send("hello books");
-})
+/*app.get('/books', function(req, res) {
+  res.send("hello books");
+});
+*/
 
 
-
-app.listen(port, function(err){
-	console.log("server running on port "+port);
+app.listen(port, function(err) {
+  console.log("server running on port " + port);
 });
